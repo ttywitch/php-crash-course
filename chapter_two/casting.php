547c00e7c1e438468c791564079aab565d704807
emma <dev@rainyday.dev>
2025-01-15 10:07:27 -0500

    finished chapter two

---
+++ chapter_two/casting.php
@@ -0,0 +1,13 @@
+<?php
+$scoreFloat = 55.9;
+$scoreInt = (int) $scoreFloat;
+
+print gettype($scoreFloat);
+print " scoreFloat = ";
+print $scoreFloat;
+print "\n";
+
+print gettype($scoreInt);
+print " scoreInt = ";
+print $scoreInt;
+print "\n";
---
+++ chapter_two/variable.php
@@ -0,0 +1,9 @@
+<?php
+$age = 21;
+var_dump($age);
+
+$age = NULL;
+var_dump($age);
+
+unset($age);
+var_dump($age);
